18a17ff25d87872584edb2c75988e2016ff1b194
MaurizioPilia <piliamaurizio@gmail.com>
2025-05-15 17:25:45 +0200

    Actualiza la versión a 1.1.3 en setup.py y refactoriza el método _get_filters en AuditLogBridgeMixin para utilizar objetos Q, mejorando la legibilidad y eficiencia de las consultas.

--- auditlog_bridge/mixin.py
+++ auditlog_bridge/mixin.py
@@ -1,6 +1,6 @@
 from auditlog.models import LogEntry
 from django.contrib.contenttypes.models import ContentType
-from django.db.models import Min, Case, When
+from django.db.models import Min, Case, When, Q
 from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.response import Response
@@ -19,15 +19,12 @@ class AuditLogBridgeMixin:
         correlation_ids = self._get_correlation_ids()
         cids_in_page = self.paginate_queryset(correlation_ids)
         if cids_in_page is not None:
-            filters = {
-                "cid__in": cids_in_page,
-                **self._get_filters(),
-            }
+            filters = Q(cid__in=cids_in_page) & self._get_filters()
             ordering = Case(*[When(cid=cid, then=pos) for pos, cid in enumerate(cids_in_page)])
             expedient_history = self.history_generator_model().generate(
                 logs=LogEntry.objects.select_related("actor")
-                .filter(**filters)
-                .order_by(ordering),
+                .filter(filters)
+                .order_by(ordering, "-timestamp"),
                 context=self._get_auditlog_bridge_context(),
             )
             return self.get_paginated_response(expedient_history)
@@ -38,7 +35,7 @@ class AuditLogBridgeMixin:
 
     def _get_correlation_ids(self):
         filters = self._get_filters()
-        logentry_qs = LogEntry.objects.filter(**filters)
+        logentry_qs = LogEntry.objects.filter(filters)
 
         if self.exclude_cid_starting_with:
             logentry_qs = logentry_qs.exclude(cid__startswith=self.exclude_cid_starting_with)
@@ -49,20 +46,25 @@ class AuditLogBridgeMixin:
             ).order_by('-min_timestamp').values_list('cid', flat=True)
         )
 
-    def _get_filters(self):
+    def _get_filters(self) -> Q:
         """
-        Get filters for the queryset based on the history option.
+        Obtiene los filtros para el queryset basados en la opción de historial.
+        Devuelve un objeto Q de Django.
         """
         instance = self.get_object()
+        content_type = ContentType.objects.get_for_model(model=instance.__class__)
+        
+        object_filter_component = Q(object_id=instance.id, content_type=content_type)
+
         if self.history_option == HISTORY_MODEL_OPTION_SINGLE:
-            content_type = ContentType.objects.get_for_model(instance)
-            return {
-                "object_id": self.get_object().id,
-                "content_type": content_type,
-            }
-        elif self.history_option == HISTORY_MODEL_OPTION_GROUPED:
-            return {f"additional_data__{self.history_generator_filter}": instance.id}
-        return {}
+            return object_filter_component
+        
+        if self.history_option == HISTORY_MODEL_OPTION_GROUPED:
+            filter_key = f"additional_data__{self.history_generator_filter}"
+            grouped_filter_component = Q(**{filter_key: instance.id})
+            return grouped_filter_component | object_filter_component
+        
+        return Q()
 
     def _check_configuration(self):
 
