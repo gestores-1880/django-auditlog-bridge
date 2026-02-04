0bb3470b623611157fb459e9323fdeebead63d2a
ffernandezo <ffernandez@gaa9.com>
2026-02-04 17:50:26 +0100

    Add settings to control admin log registration and disable audit logging for admin requests

--- auditlog_bridge/conf.py
+++ auditlog_bridge/conf.py
@@ -3,3 +3,7 @@ from django.conf import settings
 settings.CORRELATION_ID_MAX_AGE_SECONDS = getattr(
     settings, "CORRELATION_ID_MAX_AGE_SECONDS", 10
 )
+
+settings.REGISTER_ADMIN_LOGS = getattr(settings, "REGISTER_ADMIN_LOGS", True)
+
+settings.ADMIN_URL = getattr(settings, "ADMIN_URL", "admin/")
